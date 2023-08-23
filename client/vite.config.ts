7215bae7adcc0ec37892c56bc974b31268a72492
tranjm4 <98729052+tranjm4@users.noreply.github.com>
2023-08-23 01:58:27 -0700

    Removed proxies

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,14 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "auth/spotify": {
-        target: "https://wrapify-server-bff9ee0094f2.herokuapp.com",
-      },
-      "/api": {
-        target: "https://wrapify-server-bff9ee0094f2.herokuapp.com",
-      }
-    }
-  }
 })
